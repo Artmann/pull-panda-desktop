c07dbfde6ef1124d5ee11ae3e3c9db545cb511f1
Christoffer Artmann <artgaard@gmail.com>
2025-12-30 12:21:04 +0100

    React

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,5 @@
+import { type ReactElement } from 'react'
+
 export function App(): ReactElement {
   return <div>Hello from React</div>
 }
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -26,8 +26,13 @@
  * ```
  */
 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app/App'
 import './index.css'
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite'
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
